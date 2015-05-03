40e66f2c366fbc28f321205836fd0519477d20f0
Peter Schatz <voronwe13@gmail.com>
2015-05-03 00:07:19 -0400

    More stubs

--- SWE681Poker/src/server/PokerGame.java
+++ SWE681Poker/src/server/PokerGame.java
@@ -1,5 +1,6 @@
 package server;
 
+import swe681poker.Card;
 import swe681poker.CardDeck;
 
 public class PokerGame {
@@ -7,6 +8,7 @@ public class PokerGame {
 	CardDeck deck;
 	int dealer;
 	int playersremaining;
+	int pot;
 	public static final int firstroundmin = 10;
 	public static final int secondroundmin = 20;
 
@@ -37,7 +39,7 @@ public class PokerGame {
 		if(checkWin()) return;
 		playRiver();
 		biddingRound(secondroundmin);
-		checkWin();
+		checkWinFinal();
 	}
 
 	private void playRiver() {
@@ -51,16 +53,63 @@ public class PokerGame {
 	}
 
 	private void playFlop() {
+		Card burn = deck.dealOne(); //probably not necessary, but following poker rules
+		Card[] flop = deck.dealX(3);
+		for(Player player:players){
+			if(player != null)
+				player.sendFlop(flop);
+		}
+	}
+
+	/**
+	 * check if only one player remains; if so, notify players of the win, adjust pots, etc.
+	 * @return	true if a player won by being the last player.
+	 */
+	private boolean checkWin() {
+		if(playersremaining == 1){
+			for(int i=0; i<players.length; i++){
+				if(players[i].active){
+					notifyWin(i);
+					break;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * give pot to winning player, notify all players of winner;
+	 * @param position
+	 */
+	private void notifyWin(int position) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	private boolean checkWin() {
+	private void checkWinFinal(){
+		if(!checkWin()){
+			//TODO: check hands for each player, find best hand, notify all players of win.
+			int winposition = compareHands();
+			notifyWin(winposition);
+		}
+	}
+	
+	/**
+	 * find best hand, notify players of shown hands, return position of winning hand.
+	 * @return	position of winning hand.
+	 */
+	private int compareHands() {
 		// TODO Auto-generated method stub
-		return false;
+		return 0;
 	}
 
-	private void biddingRound(int firstroundmin2) {
+	/**
+	 * Conduct a round of bidding.  Get bids from each player, mark folded players as
+	 * inactive, until bidding is finished
+	 * @param minbet	the minimum bet for this round.
+	 */
+	private void biddingRound(int minbet) {
 		// TODO Auto-generated method stub
 		
 	}
