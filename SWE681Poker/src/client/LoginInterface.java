9738a0ad164d1db147b6c0c3898e0bd1a5c05eed
Peter Schatz <voronwe13@gmail.com>
2015-05-04 13:31:42 -0400

    Added username/password prompt to login screen.

--- SWE681Poker/src/client/LoginInterface.java
+++ SWE681Poker/src/client/LoginInterface.java
@@ -25,8 +25,10 @@ import javax.net.ssl.SSLSocketFactory;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 
@@ -43,77 +45,77 @@ public class LoginInterface {
     public String nonceString;
 
     public LoginInterface() {
-	try {
-	    System.setProperty("javax.net.ssl.trustStore", "clientTrustStore");
-	    System.setProperty("javax.net.ssl.trustStorePassword", "123456");
-	    SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory
-		    .getDefault();
-	    SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket(
-		    "localhost", 9999);
+    	try {
+    		System.setProperty("javax.net.ssl.trustStore", "clientTrustStore");
+    		System.setProperty("javax.net.ssl.trustStorePassword", "123456");
+    		SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory
+    				.getDefault();
+    		SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket(
+    				"localhost", 9999);
 
-	    InputStream inputstream = sslsocket.getInputStream();
-	    OutputStream outputstream = sslsocket.getOutputStream();
+    		InputStream inputstream = sslsocket.getInputStream();
+    		OutputStream outputstream = sslsocket.getOutputStream();
 
-	    ObjectOutputStream objectoutputstream = new ObjectOutputStream(
-		    outputstream);
-	    objectoutputstream.flush();
-	    ObjectInputStream objectinputstream = new ObjectInputStream(
-		    inputstream);
-	    // Generate the session key
-	    KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-	    keyGen.init(128);
-	    sessionKey = keyGen.generateKey();
-	    // Generate the nonce
-	    SecureRandom nonce = new SecureRandom();
-	    nonceString = nonce.toString().substring(
-		    nonce.toString().length() - 8);
-	    System.out.println("Plain text: " + nonceString);
-	    // Generate IV
-	    SecureRandom random = new SecureRandom();
-	    String randomString = random.toString().substring(
-		    nonce.toString().length() - 8);
-	    randomString += randomString;
-	    byte[] iv = randomString.getBytes();
-	    while (iv.length != 16) {
-		random = new SecureRandom();
-		randomString = random.toString().substring(
-			nonce.toString().length() - 8);
-		randomString += randomString;
-		iv = randomString.getBytes();
-	    }
-	    // Create the AES message object
-	    AesMessageStructure aesMessage = new AesMessageStructure(
-		    sessionKey, nonceString, randomString);
-	    // Send the AES message object
-	    objectoutputstream.writeObject(aesMessage);
-	    // Listen to server
-	    byte[] encryptedStringByteFromServer = (byte[]) objectinputstream
-		    .readObject();
-	    // reinitialize the cipher for decryption
-	    IvParameterSpec ivspec = new IvParameterSpec(iv);
-	    Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-	    cipher.init(Cipher.DECRYPT_MODE, sessionKey, ivspec);
+    		ObjectOutputStream objectoutputstream = new ObjectOutputStream(
+    				outputstream);
+    		objectoutputstream.flush();
+    		ObjectInputStream objectinputstream = new ObjectInputStream(
+    				inputstream);
+    		// Generate the session key
+    		KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+    		keyGen.init(128);
+    		sessionKey = keyGen.generateKey();
+    		// Generate the nonce
+    		SecureRandom nonce = new SecureRandom();
+    		nonceString = nonce.toString().substring(
+    				nonce.toString().length() - 8);
+    		System.out.println("Plain text: " + nonceString);
+    		// Generate IV
+    		SecureRandom random = new SecureRandom();
+    		String randomString = random.toString().substring(
+    				nonce.toString().length() - 8);
+    		randomString += randomString;
+    		byte[] iv = randomString.getBytes();
+    		while (iv.length != 16) {
+    			random = new SecureRandom();
+    			randomString = random.toString().substring(
+    					nonce.toString().length() - 8);
+    			randomString += randomString;
+    			iv = randomString.getBytes();
+    		}
+    		// Create the AES message object
+    		AesMessageStructure aesMessage = new AesMessageStructure(
+    				sessionKey, nonceString, randomString);
+    		// Send the AES message object
+    		objectoutputstream.writeObject(aesMessage);
+    		// Listen to server
+    		byte[] encryptedStringByteFromServer = (byte[]) objectinputstream
+    				.readObject();
+    		// reinitialize the cipher for decryption
+    		IvParameterSpec ivspec = new IvParameterSpec(iv);
+    		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+    		cipher.init(Cipher.DECRYPT_MODE, sessionKey, ivspec);
 
-	    // decrypt the message
-	    byte[] decrypted = cipher.doFinal(encryptedStringByteFromServer);
-	    System.out.println("Plaintext: " + new String(decrypted) + "\n");
+    		// decrypt the message
+    		byte[] decrypted = cipher.doFinal(encryptedStringByteFromServer);
+    		System.out.println("Plaintext: " + new String(decrypted) + "\n");
 
-	    if (nonceString.equals(new String(decrypted))) {
-		System.out.println("Client verified the server\n");
-		Socket socket = new Socket("localhost", 9998); 
-		// new Thread(new ClientThread(socket, sessionKey,
-		// new IvParameterSpec(iv))).start();
-		cipher.init(Cipher.ENCRYPT_MODE, sessionKey, ivspec);
-		byte[] encryptedStringByte = cipher.doFinal(new String("Bu bir deneme kaydidir\n")
-			.getBytes());
-		ObjectOutputStream objectoutputstreamTemp = new ObjectOutputStream(
-			    socket.getOutputStream());
-		objectoutputstreamTemp.writeObject(encryptedStringByte);
-	    }
+    		if (nonceString.equals(new String(decrypted))) {
+    			System.out.println("Client verified the server\n");
+    			Socket socket = new Socket("localhost", 9998); 
+    			// new Thread(new ClientThread(socket, sessionKey,
+    			// new IvParameterSpec(iv))).start();
+    			cipher.init(Cipher.ENCRYPT_MODE, sessionKey, ivspec);
+    			byte[] encryptedStringByte = cipher.doFinal(new String("Bu bir deneme kaydidir\n")
+    			.getBytes());
+    			ObjectOutputStream objectoutputstreamTemp = new ObjectOutputStream(
+    					socket.getOutputStream());
+    			objectoutputstreamTemp.writeObject(encryptedStringByte);
+    		}
 
-	} catch (Exception exception) {
-	    exception.printStackTrace();
-	}
+    	} catch (Exception exception) {
+    		exception.printStackTrace();
+    	}
     }
 
     /*
@@ -129,46 +131,74 @@ public class LoginInterface {
      * @param args
      */
     public static void main(String[] args) {
-	try {
-	    LoginInterface window = new LoginInterface();
-	    window.open();
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+    	try {
+    		LoginInterface window = new LoginInterface();
+    		window.open();
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
     }
 
     /**
      * Open the window.
      */
     public void open() {
-	Display display = Display.getDefault();
-	createContents();
-	shell.open();
-	shell.layout();
-	while (!shell.isDisposed()) {
-	    if (!display.readAndDispatch()) {
-		display.sleep();
-	    }
-	}
+    	Display display = Display.getDefault();
+    	createContents();
+    	shell.open();
+    	shell.layout();
+    	while (!shell.isDisposed()) {
+    		if (!display.readAndDispatch()) {
+    			display.sleep();
+    		}
+    	}
     }
 
     /**
      * Create contents of the window.
      */
     protected void createContents() {
-	shell = new Shell();
-	shell.setSize(445, 346);
-	shell.setText("Log in");
+    	final int WIDTH = 480, HEIGHT = 360;
+    	final int CENTERX = WIDTH/2, CENTERY = HEIGHT/2;
+    	shell = new Shell();
+    	shell.setSize(WIDTH, HEIGHT);
+    	shell.setText("Log in");
+    	
+    	Label title = new Label(shell, SWT.CENTER);
+    	title.setText("Welcome to SecurePoker");
+    	title.setBounds(10, 10, WIDTH - 20, 40);
+    	
+    	Label usernametxt= new Label(shell, SWT.NONE);
+    	usernametxt.setText("Username:");
+    	usernametxt.setBounds(CENTERX/2, CENTERY/2, 70, 20);
+    	Text usernamebox = new Text(shell, SWT.BORDER);
+    	usernamebox.setBounds(CENTERX/2 + 75, CENTERY/2, 100, 20);
+    	
+    	Label passwordtxt= new Label(shell, SWT.NONE);
+    	passwordtxt.setText("Password:");
+    	passwordtxt.setBounds(CENTERX/2, CENTERY/2+45, 70, 20);
+    	Text passwordbox = new Text(shell, SWT.PASSWORD | SWT.BORDER);
+    	passwordbox.setBounds(CENTERX/2 + 75, CENTERY/2+45, 100, 20);
 
-	Button btnSignUp = new Button(shell, SWT.NONE);
-	btnSignUp.addSelectionListener(new SelectionAdapter() {
-	    @Override
-	    public void widgetSelected(SelectionEvent e) {
+    	Button btnsignup = new Button(shell, SWT.NONE);
+    	btnsignup.addSelectionListener(new SelectionAdapter() {
+    		@Override
+    		public void widgetSelected(SelectionEvent e) {
 
-	    }
-	});
-	btnSignUp.setBounds(172, 139, 95, 28);
-	btnSignUp.setText("Sign Up");
+    		}
+    	});
+    	btnsignup.setBounds(140, 239, 95, 28);
+    	btnsignup.setText("Sign Up");
+    	
+    	Button btnsignin = new Button(shell, SWT.NONE);
+    	btnsignin.addSelectionListener(new SelectionAdapter() {
+    		@Override
+    		public void widgetSelected(SelectionEvent e) {
+
+    		}
+    	});
+    	btnsignin.setBounds(240, 239, 95, 28);
+    	btnsignin.setText("Log In");
 
     }
 }
